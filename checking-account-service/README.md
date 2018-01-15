847202ed04e369ffb4ad9e70a20577b9ed4e2135
rahul <rahulaga@gmail.com>
2018-01-14 17:05:14 -0800

    add part1 - user and checking service with hardcoded values

---
+++ checking-account-service/README.md
@@ -0,0 +1,9 @@
+Example Java Spring Application
+
+Copyright (C) 2017 Rahul Agarwal
+http://www.irahul.com
+
+Notes:
+*Each microservice is defined in its own Maven project
+*Using long for ids. Consider strongly typing them
+*Partially implemented services and tests
---
+++ checking-account-service/src/test/java/com/irahul/tbtf/http/CheckingAccountResourceTest.java
@@ -0,0 +1,67 @@
+package com.irahul.tbtf.http;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.irahul.tbtf.http.entity.HttpCheckingAccount;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class CheckingAccountResourceTest {
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	public void testGetAccountById() throws Exception {
+		MvcResult mockResponse = mockMvc.perform(get("/checking/123").accept(MediaType.APPLICATION_JSON)).andDo(print())
+				.andReturn();
+		assertThat(mockResponse.getResponse().getStatus()).isEqualTo(HttpStatus.OK.value());
+
+		String expectedResponseBody = new String(Files.readAllBytes(Paths.get("src/test/resources/checking-id.json")));
+		String mvcResponse = new String(mockResponse.getResponse().getContentAsByteArray());
+		JSONAssert.assertEquals(expectedResponseBody, mvcResponse, true);
+	}
+
+	@Test
+	public void testCreateAccount() throws Exception {
+		MvcResult mockResponse = mockMvc.perform(post("/checking").accept(MediaType.APPLICATION_JSON).content(getNewUser())
+				.contentType(MediaType.APPLICATION_JSON)).andDo(print()).andReturn();
+		assertThat(mockResponse.getResponse().getStatus()).isEqualTo(HttpStatus.CREATED.value());
+
+		String expectedResponseBody = new String(Files.readAllBytes(Paths.get("src/test/resources/checking-create.json")));
+		String mvcResponse = new String(mockResponse.getResponse().getContentAsByteArray());
+		JSONAssert.assertEquals(expectedResponseBody, mvcResponse, true);
+	}
+
+	private String getNewUser() throws JsonProcessingException {
+		ObjectMapper mapper = new ObjectMapper();
+		mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+
+		HttpCheckingAccount account = new HttpCheckingAccount();
+		account.ownerId=8899;
+		account.currency="USD";
+		account.balance=0;
+		return mapper.writeValueAsString(account);
+	}
+}
