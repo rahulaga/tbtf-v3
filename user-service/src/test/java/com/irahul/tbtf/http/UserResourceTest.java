c40b3f0c07db69a927779e320a56d61c61c092c8
rahul <rahulaga@gmail.com>
2018-01-14 17:25:15 -0800

    test updates

--- user-service/src/test/java/com/irahul/tbtf/http/UserResourceTest.java
+++ user-service/src/test/java/com/irahul/tbtf/http/UserResourceTest.java
@@ -14,7 +14,6 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.skyscreamer.jsonassert.JSONAssert;
@@ -27,8 +26,6 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.JsonProcessingException;
